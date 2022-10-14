36852b87549ecfa21636778144e4a36bc03e96b7
Paulo Fernandez <paulo.fzmz@proton.me>
2022-10-14 15:49:28 -0300

    Pipe heat loss macro Ok, pipe flow macro sketched

--- src/menu.ts
+++ src/menu.ts
@@ -1,18 +1,20 @@
 function onOpen() {
   let ui = SpreadsheetApp.getUi()
   ui.createMenu('PSR')
-    .addSubMenu(ui.createMenu('Boards')
-      .addItem('Parse Selected', 'parseBoard')
-      .addItem('Validate Radiator', 'validateRadiator')
-      .addItem('Update Radiator Data', 'updateRadiator'))
+    .addSubMenu(ui.createMenu('Radiadores')
+      .addItem('Validar', 'validateRadiator')
+      .addItem('Actualizar Datos', 'updateRadiator'))
+    .addSubMenu(ui.createMenu('Piping')
+      .addItem('Actualizar Pérdida de Calor', 'updatePipeHeatLoss')
+      .addItem('Actualizar Régimen de Flujo', 'updatePipeFlowState'))
     .addToUi();
 
 }
 
 /**
- * Setting board parser 
+ * Test board parser 
  */
-function parseBoard() {
+function testParseBoard() {
   let sheet = SpreadsheetApp.getActiveSheet();
   let range = SpreadsheetApp.getActiveRange();
 
@@ -58,4 +60,89 @@ function updateRadiator() {
   }
 
   range.setValues(values);
+}
+
+/**
+ * Update heat loss on piping
+ */
+function updatePipeHeatLoss() {
+  let conditionsRange = InterfaceUtils.rangeByName("CondicionesSistemaCalefaccion");
+  let pipeDiameterRange = InterfaceUtils.rangeByName("PipeSectionDiameter");
+  let pipeInsulationRange = InterfaceUtils.rangeByName("PipeSectionHeat")
+
+  let conditions = StatusBoard.parseBoard(conditionsRange).sections
+                    .filter(s => s.title == "Condiciones" )[0];
+
+  let tempHigh = conditions.properties
+                    .filter(p => p.name == "Temperatura Superior")[0].value;
+  
+  let tempLow = conditions.properties
+                    .filter(p => p.name == "Temperatura Inferior")[0].value;
+
+  let tempEx = conditions.properties
+                    .filter(p => p.name == "Temperatura Exterior")[0].value;
+  
+  let PPRpressure = conditions.properties
+                    .filter(p => p.name == "PPR Presión Nominal")[0].value;
+
+  if (typeof tempHigh != 'number' || 
+      typeof tempLow != 'number' || 
+      typeof tempEx != 'number' ||
+      typeof PPRpressure != 'string') {
+    throw 'Something happend with system conditions';
+  }
+
+  if (PPRpressure != "PN20" && PPRpressure != "PN16") {
+    throw 'PPR Nominal Pressure must be PN16 or PN20';
+  }
+
+  let deltaTempHigh = tempHigh - tempEx;
+  let deltaTempLow = tempLow - tempEx;
+
+  let pipeValues = pipeDiameterRange.getValues();
+  let insulatedValues = pipeInsulationRange.getValues();
+
+  for (let i = 2; i < pipeValues.length; i++) {
+    if (InterfaceUtils.isEmptyLine(pipeValues[i]) ||
+        pipeValues[i][0].length != pipeValues[i][1].length ||
+        insulatedValues[i][0] == "") {
+      continue;
+    }
+  
+    let diameter = pipeValues[i][2];
+    let insulationThickness = insulatedValues[i][0];
+
+    insulatedValues[i][1] = pprHeatLoss(PPRpressure, diameter, deltaTempHigh, insulationThickness);
+    insulatedValues[i][2] = pprHeatLoss(PPRpressure, diameter, deltaTempLow, insulationThickness);
+  }
+
+  pipeInsulationRange.setValues(insulatedValues);
+}
+
+function updatePipeFlowState() {
+  let conditionsRange = InterfaceUtils.rangeByName("CondicionesSistemaCalefaccion");
+  let pipeDiameterRange = InterfaceUtils.rangeByName("PipeSectionDiameter");
+  let pipeFlowRange = InterfaceUtils.rangeByName("PipeSectionFlowState");
+
+  let PPRpressure = StatusBoard.parseBoard(conditionsRange).sections
+    .filter(s => s.title == "Condiciones")[0].properties
+    .filter(p => p.name == "PPR Presión Nominal")[0].value;
+
+  if (PPRpressure != "PN20" && PPRpressure != "PN16") {
+    throw 'PPR Nominal Pressure must be PN16 or PN20';
+  }
+  
+  let pipeDiameterValues = pipeDiameterRange.getValues();
+  let pipeFlowValues = pipeFlowRange.getValues();
+
+  for (let i = 2; i < pipeDiameterValues.length; i++) {
+    if (InterfaceUtils.isEmptyLine(pipeDiameterValues[i])) {
+      continue;
+    }
+
+    let flow = pipeFlowValues[i][0];
+    let diameter = pipeDiameterValues[i][2];
+
+    // Not finished
+  }
 }
