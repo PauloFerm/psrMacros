f8fdd0a6a0a26fa5a341801fbcc669f5fd330cec
Paulo Fernandez <paulo.fernandez@SSAYSEN.CL>
2022-10-17 12:57:07 -0300

    test for formulas and values on selected range

--- src/test/test.pipe.ts
+++ src/test/test.pipe.ts
@@ -5,3 +5,14 @@ function test(description: string, action: any, value: any) {
 function testPressures() {
   Logger.log(pprPressureLoss("PN20", 32, 0.4));
 }
+
+function testFormula() {
+  let range = SpreadsheetApp.getActiveRange();
+
+  let values = range.getValues();
+  let formulas = range.getFormulas();
+
+  Logger.log(values);
+  Logger.log(formulas);
+  Logger.log(formulas[0][0] == "")
+}
