02669c874d326353fc809ec7e576d2e9a977505d
Paulo <paulo.fzmz@proton.me>
2023-10-04 11:59:53 -0300

    test command

--- test/math.test.ts
+++ test/math.test.ts
@@ -1,15 +1,15 @@
-//import {MathUtils} from "../src/0-physics/0-mathUtils";
+import {MathUtils} from "../src/0-physics/0-mathUtils";
 
-// describe("Math Utils Tests", () => {
-//   let vec0 = [0, 0, 0, 0, 0];
-//   let vec1 = [1, 2, 3, 4, 5];
-//   test("Dot Product: keep 0", () => {
-//     expect(MathUtils.dotProduct(vec0, vec0)).toBe(0);
-//   });
-//   test("Dot Product: to 0", () => {
-//     expect(MathUtils.dotProduct(vec0, vec1)).toBe(0);
-//   });
-//   test("Dot Product: square", () => {
-//     expect(MathUtils.dotProduct(vec1, vec1)).toBe(55);
-//   });
-// });
+describe("Math Utils Tests", () => {
+  let vec0 = [0, 0, 0, 0, 0];
+  let vec1 = [1, 2, 3, 4, 5];
+  test("Dot Product: keep 0", () => {
+    expect(MathUtils.dotProduct(vec0, vec0)).toBe(0);
+  });
+  test("Dot Product: to 0", () => {
+    expect(MathUtils.dotProduct(vec0, vec1)).toBe(0);
+  });
+  test("Dot Product: square", () => {
+    expect(MathUtils.dotProduct(vec1, vec1)).toBe(55);
+  });
+});
