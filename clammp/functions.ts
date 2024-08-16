d31e4236ee1239b7268851051053aef2ca66568b
Paulo <paulo.fzmz@proton.me>
2024-08-16 15:58:59 -0400

    menu update

--- clammp/functions.ts
+++ clammp/functions.ts
@@ -1,61 +1,70 @@
 import { readFileSync, writeFileSync } from "fs";
 import { exec } from "child_process";
 
-export interface Project { name: string, sheetId: string, scriptId: string };
-export interface ClaspFile { scriptId: string, rootDir: string };
+export interface Project {
+  name: string;
+  sheetId: string;
+  scriptId: string;
+}
+export interface ClaspFile {
+  scriptId: string;
+  rootDir: string;
+}
 
 export const currentProject = () => {
-    const projects: Project[] = require('./projectsIds.json');
-    const clasp: ClaspFile = require('../.clasp.json');
+  const projects: Project[] = require("./projectsIds.json");
+  const clasp: ClaspFile = require("../.clasp.json");
 
-    let current = projects.find(project => project.scriptId == clasp.scriptId);
+  let current = projects.find((project) => project.scriptId == clasp.scriptId);
 
-    if (current == undefined) { 
-        throw new Error("Unregistered Project"); 
-    }
+  if (current == undefined) {
+    throw new Error("Unregistered Project");
+  }
 
-    return current
-}
+  return current;
+};
 
 const overwriteId = (path: string, scriptId: string) => {
-    let data = readFileSync(path, { encoding: 'utf-8' }); 
-    let idRegex = /"scriptId":"[a-zA-Z0-9_-]*"/;
-    let newData = data.replace(idRegex, `"scriptId":"${scriptId}"`);
-    
-    writeFileSync(path, newData, 'utf-8');
-}
+  let data = readFileSync(path, { encoding: "utf-8" });
+  let idRegex = /"scriptId":"[a-zA-Z0-9_-]*"/;
+  let newData = data.replace(idRegex, `"scriptId":"${scriptId}"`);
+
+  writeFileSync(path, newData, "utf-8");
+};
 
 export const changeClaspProject = (scriptId: string) => {
-    // Executed in root directory
-    overwriteId('./.clasp.json', scriptId);
-    overwriteId('./gs/.clasp.json', scriptId);
-}
+  // Executed in root directory
+  overwriteId("./.clasp.json", scriptId);
+  overwriteId("./gs/.clasp.json", scriptId);
+};
 
 const runCommand = async (cmd: string) => {
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error: ${error.message}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-    });
-}
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+};
 
 export const updateCurrentProject = () => {
-    let command = "clasp push && cd gs && clasp pull && cd ..";
-    runCommand(command);
-}
+  let command = "clasp push && cd gs && clasp pull && cd ..";
+  runCommand(command);
+};
 
 export const deployToAllProjects = async () => {
-    const projects: Project[] = require('./projectsIds.json');
+  const projects: Project[] = require("./projectsIds.json");
 
-    projects.forEach((project) => {
-        changeClaspProject(project.scriptId);
-        runCommand("clasp push");
-    });
-}
+  projects.forEach((project) => {
+    changeClaspProject(project.scriptId);
+    runCommand("clasp push");
+  });
+};
 
 export const openSheet = (project: Project) => {
-    runCommand(`explorer https://docs.google.com/spreadsheets/d/${project.sheetId}`);
-}
+  runCommand(
+    `explorer https://docs.google.com/spreadsheets/d/${project.sheetId}`
+  );
+};
